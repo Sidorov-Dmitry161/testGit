b931039dfc919c5f2b0f158c641b0ff91f2fed54
Dmitry <dima.arm90@gmail.com>
2020-08-02 12:42:09 +0300

    commit_4 Branch forTest-3

--- src/test_2.java
+++ src/test_2.java
@@ -1,13 +1,8 @@
 public class test_2 {
     public static void main(String[] args) {
-        System.out.println("Hello git 2!");
-        int a = 0;
-        System.out.println("Learn git" + a);
-        System.out.println("Hello git 3!");
-        System.out.println("Hello git 3!");
-        System.out.println("Hello git 3!");
-        System.out.println("Hello git 3!");
-        System.out.println("Hello git 3!");
-        System.out.println("Hello git 3!");
+        System.out.println("Hello git 4!");
+        int a = 4;
+        boolean b = true;
+
     }
 }
