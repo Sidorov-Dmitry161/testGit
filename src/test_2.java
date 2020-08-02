c50d2ffcdf10b97bf131f2ffa5407e19d693452e
Dmitry <dima.arm90@gmail.com>
2020-08-02 12:31:02 +0300

    commit_2 for Test2

---
+++ src/test_2.java
@@ -0,0 +1,7 @@
+public class test_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello git 2!");
+        int a = 5;
+        System.out.println("Learn git" + a);
+    }
+}
